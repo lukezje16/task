d93d831fd4a946f7759f3456f31d47cb8377e998
s mirza <sairamirza488@gmail.com>
2024-09-13 11:59:00 +0100

    updated

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,28 +1,29 @@
-	 $('#btnRun').click(function() {
+$('#btnRun').click(function() {
 
-		$.ajax({
-			url: "libs/php/getCountryInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				lat: $('#latitude').val(),
-				lng: $('#logitude').val(),
-		
-			},
-			success: function(result) {
+	$.ajax({
+		url: "libs/php/getCountryInfo.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			lat: $('#latitude').val(),
+			lng: $('#logitude').val(),
+		},
+		success: function(result) {
 
-				console.log(JSON.stringify(result));
-		
-				if (result.status.name == "ok") {
-		
-					$('#txtLatitude').html(result['data'][0]['latitude']);
-					$('#txtLongitude').html(result['data'][0]['longitude']);
-				}
-				
-			},
-			error:function(jqHRX, textSatus, errorThrown){
-				console.log("AJAX failed");
+			console.log(JSON.stringify(result));
+
+			if (result.status.name == "ok") {
+				$('#txtLatitude').html(result['data'][0]['latitude']);
+				$('#txtLongitude').html(result['data'][0]['longitude']);
+					
 			}
-		}); 
-	 
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			// your error code
+			console.log("AJAX failed:" );
+		}
 	}); 
+
+});
+
--- libs/php/getCountryInfo.php
+++ libs/php/getCountryInfo.php
@@ -1,56 +1,62 @@
-<?php
-/*
-// first api
-$ch = curl_init();
- 
-$url='http://api.geonames.org/hierarchyJSON?';
+<?php 
+
+//ocean api
+
+    ini_set('display_errors', 'On');
+
+    error_reporting(E_ALL);
 
  
 
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $executionStartTime = microtime(true);
 
-$result=curl_exec($ch);
-
-$result = json_decode($result,true);	
-
-echo json_encode($result); 
-
-curl_close($ch);
  
-    //second api
+
+    $url='http://api.geonames.org/oceanJSON?&lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=skwembeprsmm488 &style=full';
+
+ 
+
     $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($ch, CURLOPT_URL,$url);
+
  
-	$url='http://api.geonames.org/postalCodeLookupJSON';
-	
-	curl_setopt($ch, CURLOPT_URL,$url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	$result=curl_exec($ch);
+    $result=curl_exec($ch);
 
-	$data = json_decode($result,true);	
-
-	echo json_encode($result); 
-
-	curl_close($ch);
-*/
-
-	//third api
-	$ch = curl_init();
  
-	$url = 'http://api.geonames.org/oceanJSON?formatted=true&latitude=' . $_REQUEST['latitude'] . '&longitude=' . $_REQUEST['longitude'] . '&username=smm488 &style=full';
 
-	 
-	
-	curl_setopt($ch, CURLOPT_URL,$url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_close($ch);
 
-	$result=curl_exec($ch);
-
-	$result = json_decode($result,true);	
-
-	echo json_encode($result); 
-
-	curl_close($ch);
  
+
+    $decode = json_decode($result,true);    
+
+ 
+
+    $output['status']['code'] = "200";
+
+    $output['status']['name'] = "ok";
+
+    $output['status']['description'] = "success";
+
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+    $output['data'] = $decode;
+
+   
+
+    header('Content-Type: application/json; charset=UTF-8');
+
+ 
+
+    echo json_encode($output);
+
+ 
+
 ?>
+
